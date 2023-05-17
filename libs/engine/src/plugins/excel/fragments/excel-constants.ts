ffe346796cf15e78b322c940543afb395f1274d6
nlui11 <niklas.luithardt@icloud.com>
2023-05-17 14:25:01 +0200

    Gruppe B: Aufrunden Funktion implementiert DELTA-Funktion implementiert FAKULTÄT-Funktion implementiert FALSCH-Funktion implementiert GGANZZAHL-Funktion implementiert GGT-Funktion implementiert GRAD implementiert

--- libs/engine/src/plugins/excel/fragments/excel-constants.ts
+++ libs/engine/src/plugins/excel/fragments/excel-constants.ts
@@ -258,29 +258,5 @@ const excelConstantsFragment = new PluginFragment()
     )
 // Ende Funktionen Gruppe C - Lukas
 
-// Beginn Funktionen Gruppe B
-function faculty(n) {
-    let result = 1;
-    for (let i = 2; i <= n; i++) {
-        result *= i;
-    }
-    return result;
-}
-
-const facultyHeader: FunctionHeaderItem[] = [{ name: 'n', type: 'number', evaluate: true }];
-
-const facultyFunction = new PluginFragment().addFunction(
-    'faculty',
-    facultyHeader,
-    'Returns the faculty of a number.',
-    'Gibt die Fakultät einer Zahl zurück.',
-    ({ getParameter, runtimeError }) => {
-        const n = (<NumberNode>getParameter('n')).value;
-        if (!Number.isInteger(n) || n < 0) {
-            throw runtimeError('Function only works with non-negative integers.');
-        }
-        return createNumberNode(faculty(n));
-    },
-);
 
 export default excelConstantsFragment;
